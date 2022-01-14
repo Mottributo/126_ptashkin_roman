c1a9417decc9f8852aeba36e26bed20c08acb106
Roman Ptashkin <roman_roman@tuta.io>
2022-01-15 00:29:48 +0300

    Made the interface initializer

---
+++ task_10/src/Main.java
@@ -0,0 +1,12 @@
+import core.DealLedger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DealLedger dealLedger = new DealLedger();
+        Scanner scanner = new Scanner(System.in);
+        UserInterface ui = new UserInterface(scanner, dealLedger);
+        ui.addDealDialogue("firstTime");
+        ui.commandSelection("dealLedgerLevel");
+    }
+}
