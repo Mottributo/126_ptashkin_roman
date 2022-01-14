7fead7c5cda85c941aa7fa0b100688a8ed7f966d
Roman Ptashkin <roman_roman@tuta.io>
2022-01-14 23:56:32 +0300

    Added a try/catch
    to make the method return false under every exception circumstance

--- task_10/src/core/DealLedger.java
+++ task_10/src/core/DealLedger.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 // String[] getAllDealsID()
 
 public class DealLedger {
+    
     private HashMap<String, Deal> children = new HashMap<String, Deal>();
 
     public DealLedger() {}
@@ -37,22 +38,25 @@ public class DealLedger {
         return children.keySet().toArray(new String[0]);
     }
     protected static boolean isDateCorrect(String stringDate) {
-        char[] yearChar = new char[4];
-        char[] monthChar = new char[2];
-        char[] dayChar = new char[2];
-        stringDate.getChars(0, 4, yearChar, 0);
-        stringDate.getChars(4, 6, monthChar, 0);
-        stringDate.getChars(6, 8, dayChar, 0);
-        int year = Integer.parseInt(String.valueOf(yearChar));
-        int month = Integer.parseInt(String.valueOf(monthChar));
-        int day = Integer.parseInt(String.valueOf(dayChar));
-
         try {
-            LocalDate.of(year, month, day);
+            char[] yearChar = new char[4];
+            char[] monthChar = new char[2];
+            char[] dayChar = new char[2];
+            stringDate.getChars(0, 4, yearChar, 0);
+            stringDate.getChars(4, 6, monthChar, 0);
+            stringDate.getChars(6, 8, dayChar, 0);
+            int year = Integer.parseInt(String.valueOf(yearChar));
+            int month = Integer.parseInt(String.valueOf(monthChar));
+            int day = Integer.parseInt(String.valueOf(dayChar));
+            try {
+                LocalDate.of(year, month, day);
+            } catch (Exception ex) {
+                return false;
+            }
+            return true;
         }
         catch (Exception ex) {
             return false;
         }
-        return true;
     }
 }
